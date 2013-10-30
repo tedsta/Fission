032949479052228baa56a0dd0cc2e935e09d843f
Theodore DeRego <tderego94@gmail.com>
2013-10-29 22:53:57 -1000

    VS is weird

--- include/Core/Component.h
+++ include/Core/Component.h
@@ -14,6 +14,7 @@ typedef unsigned int TypeBits;
 class Component : public RefCounted
 {
     public:
+        Component(){}
         virtual ~Component(){}
 
         /// \brief Serialize this component.
