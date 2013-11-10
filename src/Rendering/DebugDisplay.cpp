720740a3454ff6c755b3cebfda490063b9610604
Theodore DeRego <tderego94@gmail.com>
2013-11-10 07:46:24 -1000

    Debug display

---
+++ src/Rendering/DebugDisplay.cpp
@@ -0,0 +1,29 @@
+#include "Rendering/DebugDisplay.h"
+
+#include <SFML/Graphics/Text.hpp>
+
+DebugDisplay::DebugDisplay(sf::Font* font) : mFont(font)
+{
+    //ctor
+}
+
+DebugDisplay::~DebugDisplay()
+{
+    //dtor
+}
+
+void DebugDisplay::render(sf::RenderTarget& target)
+{
+    int y = 0;
+    for (auto it : mMessages)
+    {
+        sf::Text msg;
+        msg.setFont(*mFont);
+        msg.setString(it.first+": "+it.second);
+        msg.setCharacterSize(24);
+        msg.setColor(sf::Color::White);
+        msg.setPosition(5, y);
+        target.draw(msg);
+        y += 30;
+    }
+}
