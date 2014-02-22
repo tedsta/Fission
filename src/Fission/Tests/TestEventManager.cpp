0ed90e71d85ee9348e75424b28bd22427dbf0f27
Theodore DeRego <tderego94@gmail.com>
2014-02-22 00:45:59 -1000

    Improved EventManager unit test

--- src/Fission/Tests/TestEventManager.cpp
+++ src/Fission/Tests/TestEventManager.cpp
@@ -14,6 +14,14 @@ struct TestEvent : public EventData
     int data;
 };
 
+struct TestEvent2 : public EventData
+{
+    TestEvent2(const std::string& str, const std::string& data) : str(str), data(data) {}
+
+    std::string str;
+    std::string data;
+};
+
 class TestListener
 {
     public:
@@ -25,9 +33,9 @@ class TestListener
                 test1 = true;
         }
 
-        void onTestEvent2(const TestEvent& event)
+        void onTestEvent2(const TestEvent2& event)
         {
-            if (event.str == "hello" && event.data == 42)
+            if (event.str == "hello" && event.data == "world")
                 test2 = true;
         }
 
@@ -42,10 +50,11 @@ TEST(EventManager_fireEventMemberFunc)
     TestListener listener1;
     TestListener listener2;
 
-    eventMgr.addListener<TestEvent>(&TestListener::onTestEvent, listener1);
-    eventMgr.addListener<TestEvent>(&TestListener::onTestEvent2, listener2);
+    eventMgr.addListener(&TestListener::onTestEvent, listener1);
+    eventMgr.addListener(&TestListener::onTestEvent2, listener2);
 
     eventMgr.fireEvent(TestEvent("hello", 42));
+    eventMgr.fireEvent(TestEvent2("hello", "world"));
 
     CHECK(listener1.test1 && !listener1.test2);
     CHECK(!listener2.test1 && listener2.test2);
